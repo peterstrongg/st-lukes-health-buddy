52df0eb4e04014aace586f6575932860f52f8cec
Peter Strong <peterstrongdev@outlook.com>
2024-03-08 11:38:10 -0500

    Preparation for database connection

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
 from flask import (
     Flask,
-    render_template
+    render_template,
+    jsonify,
+    abort
 )
 from flask_cors import CORS
-
 from database import database
+
 
 app = Flask(__name__, static_folder="client/build/static", template_folder="client/build")
 CORS(app)
 
+
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def home(path):
-    # db = database.Database()
     return render_template("index.html")
+
+
+@app.route("/api/v1/training/<path:module>", methods=["GET"])
+def serve_training_module(module):
+    db = database.Database()
+    data = db.get_data_by_title(module)
+
+    if data:
+        return jsonify(data)
+    else:
+        return abort(404)
+
 
 if __name__ == "__main__":
     app.run()
--- database/database.py
+++ database/database.py
@@ -2,10 +2,21 @@ import psycopg2
 
 class Database:
     def __init__(self):
-        self.conn = psycopg2.connect(
-            database="",
-            user="",
-            password="",
-            host="",
-            port=5432
-        )
+        # self.conn = psycopg2.connect(
+        #     database="",
+        #     user="",
+        #     password="",
+        #     host="",
+        #     port=5432
+        # )
+
+        pass
+
+    def get_data_by_title(self, module):
+        if module == "diabetes-education":
+            return ({
+                "video" : "https://player.vimeo.com/video/552139023?h=cd454073fe", 
+                "description" : "Diabetes Education"
+            })
+        
+        return False
