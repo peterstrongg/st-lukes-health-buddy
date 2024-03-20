27a1636ab50ca283003fe2c11569226409b371b8
JoelTul <128608362+JoelTul@users.noreply.github.com>
2024-03-20 19:54:07 -0400

    Basic Description Box (Joel is a css wizard)

--- client/src/components/DiabetesEducation/DiabetesEducation.js
+++ client/src/components/DiabetesEducation/DiabetesEducation.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 import './DiabetesEducation.css';
 
 function DiabetesEducation() {
+  // Description text to be fetched from the database later
+  const descriptionText = "This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.This is the description for the Diabetes Education module.";
+
   return (
     <div className="diabeteseducation-container">
       <h1>Diabetes Education</h1>
@@ -21,11 +24,16 @@ function DiabetesEducation() {
         <a href="https://vimeo.com/552139023">Module A - Pediatric Diabetes Education</a> from{' '}
         <a href="https://vimeo.com/stlukes">St. Luke's</a> on <a href="https://vimeo.com">Vimeo</a>.
       </p>
-      {/* Add a Link component for navigating back to the splash screen */}
+      {/* Description box with green border */}
+      <div className="description-container">
+        <div className="green-box">
+          <p>{descriptionText}</p>
+        </div>
+      </div>
+      {/* Link to navigate back to the splash screen */}
       <Link to="/"><button>Go back to Splash</button></Link>
     </div>
   );
 }
 
 export default DiabetesEducation;
-
