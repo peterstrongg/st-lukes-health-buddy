166424de9da38140e95c3cdf431dfbc8c1e1ae35
R-E-Miller <129326901+R-E-Miller@users.noreply.github.com>
2024-03-20 11:37:25 -0400

    Create connect.js
    Create DB connection script

---
+++ client/public/connect.js
@@ -0,0 +1,20 @@
+const { Pool } = require('pg');
+
+// Create pool with creds
+const pool = new Pool({
+  user: 'uale0h3jh7ojt6',
+  host: 'cc3engiv0mo271.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com',
+  database: 'd3hn81upsdt7ta',
+  password: 'p3abe9db54f09a88279e1f5da77add778ddd27e4285f6981d37d5ac9bc64229ed',
+  port: 5432,
+
+});
+
+// Connect to the database
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  console.log('Connection to database successful!');
+  
+});
