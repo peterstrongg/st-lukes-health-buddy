a3928990afc82619ade3f0afcf4d1af6b45e8fc7
Peter Strong <peterstrongdev@outlook.com>
2024-03-20 12:09:39 -0400

    Added db connection to backend

--- database/database.py
+++ database/database.py
@@ -1,14 +1,24 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
+PG_DB_NAME = os.getenv("PG_DB_NAME")
+PG_USER = os.getenv("PG_USER")
+PG_HOST = os.getenv("PG_HOST")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+PG_PORT = os.getenv("PG_PORT")
+
 
 class Database:
     def __init__(self):
-        # self.conn = psycopg2.connect(
-        #     database="",
-        #     user="",
-        #     password="",
-        #     host="",
-        #     port=5432
-        # )
+        self.conn = psycopg2.connect(
+            database=PG_DB_NAME,
+            user=PG_USER,
+            password=PG_PASSWORD,
+            host=PG_HOST,
+            port=PG_PORT
+        )
 
         pass
 
@@ -20,3 +30,5 @@ class Database:
             })
         
         return False
+    
+db = Database()
